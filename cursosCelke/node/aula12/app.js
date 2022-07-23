cb7a291904409347674b6fe161b1f2c2248c90aa
Gustavo H Rodrigues de Jesus <rodrigues.gustavoh@gmail.com>
2022-07-23 10:44:38 -0300

    aula12 - instalacao HANDLEBARS e criacao de rotas para paginas - OK

--- cursosCelke/node/aula12/app.js
+++ cursosCelke/node/aula12/app.js
@@ -5,13 +5,19 @@ const app = express();
 
 // a linha abaixo, no video, nao estava funcionando somente: handlebars({defaultLayout: 'main'})
 // então foi adicionado o ".engine". Desta forma, funcionou
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' })) //chama o views/layouts/main.handlebars
-// app.engine('handlebars', handlebars());
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' })) //chama o views/layouts/main.handlebars - funciona
+// app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' })) // essa era a linha no video (sem o .engine) e nao funcionou
 app.set('view engine', 'handlebars')
 
 //Rotas
+app.get('/pagamento', function(req, res){
+    // res.send("Pagina para LISTAR pagamento") // para testar se aparece no navegador. URL: http://localhost:8080/pagamento
+    res.render('pagamento') // renderiza a pagina views/pagamento.handlesbars
+})
+
 app.get('/add-pagamento', function(req, res){
-    res.send("Formulario para cadastrar pagamento")
+    // res.send("Formulario para CADASTRAR pagamento") // para testar se aparece no navegador. URL: http://localhost:8080/add-pagamento
+    res.render('add-pagamento') // renderiza a pagina views/pagamento.handlebars
 })
 
 app.listen(8080);
